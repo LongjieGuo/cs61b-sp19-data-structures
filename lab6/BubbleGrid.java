e3d0789ba3e89db79e3abb4d495be90690cfeb62
Longjie Guo <longjieguo@icloud.com>
2020-04-28 21:40:06 +0800

    ver1

--- lab6/BubbleGrid.java
+++ lab6/BubbleGrid.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
-
 public class BubbleGrid {
+    private int[][] grid;
+    private int[][] grid_map;
+    private int sizeRow;
+    private int sizeColumn;
+    private int[] bubbleFallCount;
+    private UnionFind u;
+
+    public BubbleGrid(int[][] g) {
+        grid = g;
+        sizeRow = g.length;
+        sizeColumn = g[0].length;
+        grid_map = new int[sizeRow][sizeColumn];
+        u = new UnionFind(sizeRow * sizeColumn);
+        int index = 0;
+        for (int i = 0; i < sizeRow; i++) {
+            for (int j = 0; j < sizeColumn; j++) {
+                grid_map[i][j] = index;
+                index++;
+            }
+        }
+
+        
+    }
+
+    public int[] popBubbles(int[][] darts) {
+        bubbleFallCount = new int[darts.length];
+
+        return bubbleFallCount;
+    }
+
 }
--- lab6/UnionFind.java
+++ lab6/UnionFind.java
@@ -1,34 +1,49 @@
 public class UnionFind {
 
     // TODO - Add instance variables?
+    private int[] parent;
 
     /* Creates a UnionFind data structure holding n vertices. Initially, all
        vertices are in disjoint sets. */
     public UnionFind(int n) {
         // TODO
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = -1;
+        }
     }
 
     /* Throws an exception if v1 is not a valid index. */
     private void validate(int vertex) {
         // TODO
+        if (vertex >= parent.length) {
+            throw new IllegalArgumentException();
+        }
     }
 
     /* Returns the size of the set v1 belongs to. */
     public int sizeOf(int v1) {
         // TODO
-        return -1;
+        validate(v1);
+        return -parent[find(v1)];
     }
 
     /* Returns the parent of v1. If v1 is the root of a tree, returns the
        negative size of the tree for which v1 is the root. */
     public int parent(int v1) {
         // TODO
-        return -1;
+        validate(v1);
+        return parent[v1];
     }
 
     /* Returns true if nodes v1 and v2 are connected. */
     public boolean connected(int v1, int v2) {
         // TODO
+        validate(v1);
+        validate(v2);
+        if (find(v1) == find(v2)) {
+            return true;
+        }
         return false;
     }
 
@@ -38,14 +53,39 @@ public class UnionFind {
        vertex with itself or vertices that are already connected should not 
        change the sets but may alter the internal structure of the data. */
     public void union(int v1, int v2) {
-        // TODO
+        validate(v1);
+        validate(v2);
+        if (!connected(v1, v2)) {
+            if (sizeOf(v1) > sizeOf(v2)) {
+                parent[find(v1)] -= sizeOf(v2);
+                parent[find(v2)] = find(v1);
+
+            } else {
+                parent[find(v2)] -= sizeOf(v1);
+                parent[find(v1)] = find(v2);
+            }
+        }
     }
 
     /* Returns the root of the set V belongs to. Path-compression is employed
        allowing for fast search-time. */
     public int find(int vertex) {
         // TODO
-        return -1;
+        validate(vertex);
+        int root = vertex;
+        while (parent[root] >= 0) {
+            root = parent[root];
+        }
+        /* path compression
+        while (vertex != root) {
+            int previousParent = parent(vertex);
+            parent[vertex] = root;
+            vertex = previousParent;
+        }*/
+        return root;
     }
 
+    public void fall(int vertex) {
+        parent[vertex] = -1;
+    }
 }
